df5e91cfe9df21603e1e4ddaba0e738a45a23dc7
Ievgen <zhtovtin@gmail.com>
2022-12-12 13:42:23 -0600

    check commit

--- HackerRank/3-vowels-and-consonants.js
+++ HackerRank/3-vowels-and-consonants.js
@@ -1,15 +1,17 @@
-const s = 'javascriptloops';
+const newString = 'javascriptloops';
 
-function vowelsAndConsonants(s) {
-    const n = s.length;
+function vowelsAndConsonants(newString) {
+    const newStringLength = newString.length;
     
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i < newStringLength; i++) {
         if (([i] == "a") || ([i] == "e") || ([i] == "i") || ([i] == "o") || (s[i] == "u")) {
               console.log([i]); //prints all vowels
         }
     }
 
     //TODO
+
+
 };
 
-vowelsAndConsonants();
+vowelsAndConsonants(newString);
